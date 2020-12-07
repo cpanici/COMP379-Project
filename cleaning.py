7a0c6080b2ca2e1a6652edbcb128171a9c5d82d6
Christian Panici <47305647+cpanici@users.noreply.github.com>
2020-12-06 22:38:42 -0600

    update logistic and cleaning scripts

--- cleaning.py
+++ cleaning.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 
+# Training dataset
 data = pd.read_csv('data/Test 1.csv')
 
 # Recode target variable
@@ -15,3 +16,17 @@ complete = pd.merge(numerics, dummies, left_on=numerics.index, right_on=dummies.
 complete = complete.drop('key_0', axis=1)
 
 complete.to_csv('data/complete.csv')
+
+# Unseen dataset
+data = pd.read_csv('data/Test 2.csv')
+
+data = data.drop('card_offer', axis=1)
+
+categoricals = data.select_dtypes(include='object')
+dummies = pd.get_dummies(categoricals)
+
+numerics = data.select_dtypes(exclude='object')
+complete = pd.merge(numerics, dummies, left_on=numerics.index, right_on=dummies.index)
+complete = complete.drop('key_0', axis=1)
+
+complete.to_csv('data/unseen.csv', index=False)
